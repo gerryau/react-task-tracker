9a0e557b6b6bc7aed3eca5007a87f7db5cf751a7
gerryau <gerrykorf@gmail.com>
2021-04-23 20:32:17 +1000

    UI and form completion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+import AddTask from './components/AddTask'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -16,25 +18,38 @@ function App() {
         day: 'Feb 6th at 1:30pm',
         reminder: false,
     }        
-]
-)
+  ])
 
-// Delete Task
-const deleteTask =  (id) => {
-  setTasks(tasks.filter((task) => task.id !== id))
-}
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    console.log(id)
+    const newTask = {id,...task}
+    setTasks([...tasks, newTask])
+  }
 
-// Toggle reminder
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
-}
-  
-  return (
-    <div className="container">
-      <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete ={deleteTask} toggleReminder ={toggleReminder}/> : 'There are no tasks'}
-    </div>
-  );
+  // Toggle Add Task Form
+  const toggleShowTaskForm = () => {
+    setShowAddTask(!showAddTask)
+  }
+
+  // Delete Task
+  const deleteTask =  (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+    
+    return (
+      <div className="container">
+        <Header onAdd={toggleShowTaskForm} showAdd={showAddTask}/>
+        {showAddTask && <AddTask onAdd ={addTask} display={showAddTask}/>}
+        {tasks.length > 0 ? <Tasks tasks={tasks} onDelete ={deleteTask} toggleReminder ={toggleReminder}/> : 'There are no tasks'}
+      </div>
+    );
 }
 
 export default App;
